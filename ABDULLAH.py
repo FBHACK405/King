1938d18659733f0277bc78f17b3e1552fc3841cc
FBHACK405 <113000308+FBHACK405@users.noreply.github.com>
2023-02-16 16:10:11 +0500

    Update ABDULLAH.py

--- ABDULLAH.py
+++ ABDULLAH.py
@@ -1,1 +1,13 @@
-
+import os, sys, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('xdg-open https://facebook.com/groups/3465474777107830/')
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import file64
+elif bit == '32bit':
+    import file32
+ 
