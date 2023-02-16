70c71c9d272a9fa59017295265474c60325b9d10
FBHACK405 <113000308+FBHACK405@users.noreply.github.com>
2023-02-16 21:13:10 +0500

    Update trt.py

--- trt.py
+++ trt.py
@@ -2,7 +2,6 @@ import os
 os.system('git pull')
 from os import path,system
 
-    print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
     if path.isfile("File.so"):
         pass
     else:
